dfbf6c1c845d3fcf0e662a1b9339a6d9d9d03241
OrbitTurner <orbitturner@gmail.com>
2021-02-06 20:01:28 +0000

    Console Deign

--- JS/Beautiful Console Log.js
+++ JS/Beautiful Console Log.js
@@ -11,6 +11,25 @@ console.log('%cR%ca%ci%cn%cb%co%cw',
   'font-size: 20px; color: red');
  // --------------------------------
  
+ // -------------------------------------
+// >>> ORIENTA INITIAL LOG <<<
+console.log('%cS%cT%cA%cR%cT%cI%cN%cG',
+  'font-size: 30px; color: blue;',
+  'font-size: 30px; color: lightblue;',
+  'font-size: 30px; color: lightgreen;',
+  'font-size: 30px; color: green',
+  'font-size: 30px; color: yellow;',
+  'font-size: 30px; color: orange',
+  'font-size: 30px; color: red',
+  'font-size: 30px; color: blue;');
+console.log('%c' + 'ORIENTA',
+  'font-family:Comic Sans MS; color: #fff; ' +
+  'font-size:50px; font-weight:bold; ' +
+  'background-image: repeating-linear-gradient(45deg, hsla(64,83%,54%,0.05) 0px, hsla(64,83%,54%,0.05) 1px,transparent 1px, transparent 11px,hsla(64,83%,54%,0.05) 11px, hsla(64,83%,54%,0.05) 12px,transparent 12px, transparent 32px),repeating-linear-gradient(90deg, hsla(64,83%,54%,0.05) 0px, hsla(64,83%,54%,0.05) 1px,transparent 1px, transparent 11px,hsla(64,83%,54%,0.05) 11px, hsla(64,83%,54%,0.05) 12px,transparent 12px, transparent 32px),repeating-linear-gradient(0deg, hsla(64,83%,54%,0.05) 0px, hsla(64,83%,54%,0.05) 1px,transparent 1px, transparent 11px,hsla(64,83%,54%,0.05) 11px, hsla(64,83%,54%,0.05) 12px,transparent 12px, transparent 32px),repeating-linear-gradient(135deg, hsla(64,83%,54%,0.05) 0px, hsla(64,83%,54%,0.05) 1px,transparent 1px, transparent 11px,hsla(64,83%,54%,0.05) 11px, hsla(64,83%,54%,0.05) 12px,transparent 12px, transparent 32px),linear-gradient(90deg, rgb(41, 27, 158),rgb(249, 77, 212));' +
+  ' border-radius: 5px; padding: 20px')
+// -------------------------------------
+ 
+ 
  // ===============
  https://www.aaron-powell.com/posts/2019-03-14-pretty-javascript-console/
  
