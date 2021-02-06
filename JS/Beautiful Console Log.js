10b824cc29ddd4f650358bc510c842501e4e8ee7
OrbitTurner <orbitturner@gmail.com>
2021-02-06 19:30:26 +0000

    Adding COnsole JS

---
+++ JS/Beautiful Console Log.js
@@ -0,0 +1,21 @@
+console.log('%c' + 'This console is on fire', 
+'font-family:Comic Sans MS; font-size:50px; font-weight:bold; background: linear-gradient(#f00, yellow); border-radius: 5px; padding: 20px')
+// ---------------------------------
+console.log('%cR%ca%ci%cn%cb%co%cw',
+  'font-size: 20px; color: blue;',
+  'font-size: 25px; color: lightblue;',
+  'font-size: 30px; color: lightgreen;',
+  'font-size: 35px; color: green',
+  'font-size: 30px; color: yellow;',
+  'font-size: 25px; color: orange',
+  'font-size: 20px; color: red');
+ // --------------------------------
+ 
+ // ===============
+ https://www.aaron-powell.com/posts/2019-03-14-pretty-javascript-console/
+ 
+ // ===============
+ https://medium.com/novice2pro/use-javascript-console-log-like-a-pro-8e08f5301590
+ 
+ // ===============
+ https://dev.to/wangonya/better-consolelogs-448c
